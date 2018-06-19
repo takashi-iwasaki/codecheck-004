7447f04a68e714261ac337d490c5799037f52296
tie302395 <tie302395@S1612C11975-T1.tisnt.tis.co.jp>
2018-06-19 21:28:19 +0900

    Test01

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -1,10 +1,47 @@
 package codecheck;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
 	public static void main(String[] args) {
-		for (int i = 0, l = args.length; i < l; i++) {
-			String output = String.format("argv[%s]: %s", i, args[i]);
-			System.out.println(output);
+
+		//引数3個以外はエラー
+		if (args.length != 3) {
+			System.out.println("Error01!");
+			return;
 		}
+
+		StringBuilder sb = new StringBuilder();
+
+		//値の格納用
+		Map<String, String> keyMap = new HashMap<>();
+
+		String[] params1 = args[0].split(":");
+		String[] params2 = args[1].split(":");
+
+		if (Integer.parseInt(params1[0]) <= Integer.parseInt(params2[0])) {
+
+			if (Integer.parseInt(args[2]) % Integer.parseInt(params1[0]) == 0) {
+				sb.append(params1[1]);
+			}
+			if (Integer.parseInt(args[2]) % Integer.parseInt(params2[0]) == 0) {
+				sb.append(params2[1]);
+			}
+		} else {
+			if (Integer.parseInt(args[2]) % Integer.parseInt(params2[0]) == 0) {
+				sb.append(params2[1]);
+			}
+			if (Integer.parseInt(args[2]) % Integer.parseInt(params1[0]) == 0) {
+				sb.append(params1[1]);
+			}
+		}
+
+		if (sb.length() == 0) {
+			System.out.println(args[2]);
+		} else {
+			System.out.println(sb.toString());
+		}
+
 	}
 }
